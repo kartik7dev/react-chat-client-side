0a9060f427ae63f153d05f7f332e82ca5759b39c
Kartikeyan Kothari <kartikeyank@pdcnew.iirs.gov.in>
2023-05-01 16:55:21 +0530

    client-side

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import { Routes, Route } from "react-router-dom";
+
+import Join from './components/Join/Join'
+import Chat from './components/Chat/Chat'
+import { useLocation } from "react-router-dom";
+
+const App = () => {
+  const location = useLocation()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path='/' exact element={<Join/>}></Route>
+      <Route path='/chat' element={<Chat location={location}/>}></Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
